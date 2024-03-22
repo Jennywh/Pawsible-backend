c5fa68a586100359df8261a43e3d10d21ed59576
Qifan Yang <qifany1111@gmail.com>
2024-03-21 19:27:48 -0500

    new function

--- pawsible-functions/endpoints/userProfile.js
+++ pawsible-functions/endpoints/userProfile.js
@@ -1,13 +1,16 @@
 const functions = require("firebase-functions");
 const admin = require("../utils/firebaseAdmin");
+const logger = functions.logger;
 
 exports.getProfile = functions.https.onRequest(async (request, response) => {
   if (request.method !== "GET") {
     response.status(405).send("Method Not Allowed");
     return;
+  } else if (!request.query.uid) {
+    response.status(400).send("Missing user ID");
+    return;
   }
-
-  functions.logger.info("Getting user profile", { structuredData: true });
+  logger.info("Getting user profile");
 
   try {
     const snapshot = await admin
@@ -22,7 +25,38 @@ exports.getProfile = functions.https.onRequest(async (request, response) => {
       response.status(404).send("User not found");
     }
   } catch (error) {
-    functions.logger.error("Error while getting user profile", error);
+    logger.error("Error while getting user profile", error);
     response.status(500).send("Error fetching user profile");
   }
 });
+
+exports.createOrUpdateProfile = functions.https.onRequest(
+  async (request, response) => {
+    if (request.method !== "POST") {
+      response.status(405).send("Method Not Allowed");
+      return;
+    } else if (!request.query.uid) {
+      response.status(400).send("Missing user ID");
+      return;
+    }
+    logger.info("Creating or updating user profile");
+
+    try {
+      const userProfile = request.body;
+      if (!userProfile || Object.keys(userProfile).length === 0) {
+        response.status(400).send("Missing profile data");
+        return;
+      }
+
+      const userRef = admin
+        .firestore()
+        .collection("users")
+        .doc(request.query.uid);
+      await userRef.set(userProfile, { merge: true });
+      response.status(200).send("Profile updated successfully");
+    } catch (error) {
+      logger.error("Error while creating or updating user profile", error);
+      response.status(500).send("Error processing user profile");
+    }
+  }
+);
--- pawsible-functions/index.js
+++ pawsible-functions/index.js
@@ -2,5 +2,6 @@ const userProfile = require('./endpoints/userProfile');
 const petSittingRequest = require('./endpoints/petSittingRequest');
 
 exports.getProfile = userProfile.getProfile;
+exports.createOrUpdateProfile = userProfile.createOrUpdateProfile;
 exports.getRequests = petSittingRequest.getRequests;
 exports.addRequest = petSittingRequest.addRequest;
